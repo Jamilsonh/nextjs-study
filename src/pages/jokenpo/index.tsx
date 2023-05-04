9c775551d228e829b57318ed20407065c3b33fa9
jamilsonh <2jamilson@gmail.com>
2023-05-04 20:46:41 -0300

    feat: :sparkles: Melhorando a logica da pagina jokenpo

--- src/pages/jokenpo/index.tsx
+++ src/pages/jokenpo/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
 
-const options = ['Pedra', 'Papel', 'Tesoura'];
+const options = [
+  { name: 'Pedra', emoji: '✊' },
+  { name: 'Papel', emoji: '✋' },
+  { name: 'Tesoura', emoji: '✌' },
+];
 
 const getRandomOption = () => {
   const index = Math.floor(Math.random() * options.length);
@@ -26,33 +30,74 @@ export default function Joken() {
   const [playerOption, setPlayerOption] = useState('');
   const [computerOption, setComputerOption] = useState('');
   const [result, setResult] = useState('');
+  const [selectedOptions, setSelectedOptions] = useState<{
+    player: string;
+    computer: string;
+  }>({ player: '', computer: '' });
 
-  const handleOptionClick = (option: string) => {
-    const computer = getRandomOption();
-    const gameResult = getResult(option, computer);
-    setPlayerOption(option);
-    setComputerOption(computer);
-    setResult(gameResult);
+  const handleOptionClick = (optionName: string) => {
+    const option = options.find((o) => o.name === optionName);
+    if (option) {
+      const computer = getRandomOption();
+      const gameResult = getResult(option.name, computer.name);
+      setSelectedOptions({ player: option.emoji, computer: computer.emoji });
+      setPlayerOption(option.emoji);
+      setComputerOption(computer.emoji);
+      setResult(gameResult);
+    }
   };
 
   return (
     <div className='flex items-center justify-center h-full w-full bg-gray-900'>
-      <div className='bg-gray-600 w-1/2 h-3/5 border-white border-4 rounded-3xl p-4 flex flex-col justify-center items-center text-3xl mx-auto '>
-        <h1>Escolha uma opção:</h1>
-        <div>
-          {options.map((option) => (
-            <button key={option} onClick={() => handleOptionClick(option)}>
-              {option}
-            </button>
-          ))}
-        </div>
-        {playerOption && (
-          <div>
-            <h2>Você escolheu: {playerOption}</h2>
-            <h2>O computador escolheu: {computerOption}</h2>
-            <h2>Resultado: {result}</h2>
+      <div className='bg-gray-600 w-1/2 h-3/5 border-white border-4 rounded-3xl flex justify-between items-center text-3xl mx-auto overflow-hidden '>
+        <div className='w-1/5 h-full bg-blue-300 p-2 flex flex-col justify-center items-center'>
+          <div className='h-1/5'>Voce</div>
+          <div className='h-4/5 flex flex-col justify-center items-center w-full'>
+            {options.map((option) => (
+              <button
+                className={`bg-gray-500 w-1/2 h-2/5 rounded-full hover:bg-transparent ${
+                  selectedOptions.player === option.emoji ? 'bg-blue-400' : ''
+                }`}
+                key={option.name}
+                onClick={() => handleOptionClick(option.name)}
+              >
+                {option.emoji}
+              </button>
+            ))}
           </div>
-        )}
+        </div>
+
+        <div className='w-3/5 h-full bg-blue-500 p-2 flex justify-center items-center'>
+          {playerOption && (
+            <>
+              <div className='w-1/4 h-full flex justify-center items-center'>
+                <h2>{playerOption}</h2>
+              </div>
+              <div className='w-2/4 h-full flex justify-center items-center'>
+                <h2>{result}</h2>
+              </div>
+              <div className='w-1/4 h-full flex justify-center items-center'>
+                <h2>{computerOption}</h2>
+              </div>
+            </>
+          )}
+        </div>
+
+        <div className='w-1/5 h-full bg-blue-300 p-2 flex flex-col justify-center items-center'>
+          <div className='h-1/5'>Machine</div>
+          <div className='h-4/5 flex flex-col justify-center items-center w-full'>
+            {options.map((option) => (
+              <button
+                className={`bg-gray-500 w-1/2 h-2/5 rounded-full hover:bg-transparent ${
+                  selectedOptions.computer === option.emoji ? 'bg-blue-400' : ''
+                }`}
+                key={option.name}
+              >
+                {option.emoji}
+              </button>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
